061888f6da3f9b4b84277fa10a1b5d8ecb709bb2
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2020-01-12 20:32:24 +0200

    Start. Working project created with dotnet new

---
+++ PhoenyxStudio.Omdb/Client.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PhoenyxStudio.Omdb
+{
+    public class Client
+    {
+        
+    }
+}
---
+++ omdbapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace omdbapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
